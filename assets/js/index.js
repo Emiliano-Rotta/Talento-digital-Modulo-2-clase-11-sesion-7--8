26235c7a93420e4fe69ed1b672fdd4736f3f87f4
Emiliano Rotta <emilianorottaf@gmail.com>
2024-08-13 21:32:56 -0300

    first commits

---
+++ assets/js/index.js
@@ -0,0 +1,92 @@
+//funciones
+
+// alert("Hola, espero que estes muy bien y disfrutes de la clase de hoy. Paulina")
+
+// alert("Hola, espero que estes muy bien y disfrutes de la clase de hoy. Armando")
+
+// alert("Hola, espero que estes muy bien y disfrutes de la clase de hoy. Rodrigo")
+
+// alert("Hola, espero que estes muy bien y disfrutes de la clase de hoy. Jonathan")
+
+// alert("Hola, espero que estes muy bien y disfrutes de la clase de hoy. Erick")
+
+// alert("Hola, espero que estes muy bien y disfrutes de la clase de hoy. Bernarda")
+
+// alert("Hola, espero que estes muy bien y disfrutes de la clase de hoy. Helmo")
+
+// const saludo = (nombre) => {
+//     alert("Hola, espero que estes muy bien y disfrutes de la clase de hoy. " + nombre)
+// }
+
+// saludo("Paulina")
+// saludo("Armando")
+// saludo("Rodrigo")
+// saludo("Jonathan")
+// saludo("Erick")
+// saludo("Bernarda")
+// saludo("Helmo")
+
+// const nombreApellido = (nombre, apellido, edad) => {
+//     alert("Hola, tu nombre es. " + nombre + " y tu apellido es: " + apellido)
+//     console.log(edad*4)
+// }
+
+// nombreApellido("Paulina", "Ahumada", 25)
+// nombreApellido("Armando", "Marin", 20)
+// nombreApellido("Rodrigo", "Donoso", 23)
+
+
+//Condicionales
+//llueve --> al cine  -->si llueve vamos al cine, 
+//sol --> a la playa  -->si esta soleado vamos a la playa 
+//cuaquier otro clima -->sino nos quedamos en casa
+// var clima = prompt("escriba el clima")
+
+// if(clima === "llueve") {
+//     console.log("vamos al cine")
+// } else if(clima === "soleado"){
+//     console.log("vamos a la playa")
+// } else {
+//     console.log("nos quedamos en casa")
+// }
+
+
+//Hay 3 tipos de iguales =
+
+//el primer =, es uno solo y se usa para darle el valor a una variable ejemplo:  
+//var edad = 12
+
+//cuando tenemos == es una identidad, que tiene en cuenta el valor pero no el tipo.
+
+// 1 == "1" --> verdadero
+
+//cuando tenemos === es una identidad, que tiene en cuenta el valor y el tipo.
+
+// 1 === "1" --> falso?
+
+
+// var numero = prompt()  // -> "1"
+
+// if(numero === 1) {
+//     console.log("verdadero")
+// } else {
+//     console.log("falso")
+// }
+
+//ACTIVAD: hagan una función que si recibe la palabra mujer devuelva console.log(Bienvenida) si si recibe la palabra varon devuelva console.log(Bienvenido)
+
+//Resultado:
+// const bienvenida = (genero) =>{
+//     if(genero === "mujer"){
+//         console.log("Bienvenida")
+//     }
+//     else if(genero === "varon"){
+//         console.log("Bienvenido")
+//     }
+//     else{
+//         console.log("Bienvenido/a")
+//     }
+// }
+// bienvenida("mujer")
+
+
